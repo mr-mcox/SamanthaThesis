32671f98fc77ce16d0f3e2e3ced88aa8e992ff75
Matthew Cox <matthew.cox@teachforamerica.org>
2015-09-22 21:06:25 -0500

    data_for_visualizer created

--- tests/test_assemble_data.py
+++ tests/test_assemble_data.py
@@ -191,3 +191,39 @@ def test_dimension_value_frame(mock_question_key, resp_for_overlap_dim):
         question_key=mock_question_key, survey_results=resp_for_overlap_dim)
 
     assert_frame_subset(p.dimension_value_frame('QC3', 'QC2'), exp_df)
+
+
+def mock_dim_value_frame(self, dim_code, q_code):
+    mock_c = ['Entry Id', 'dimension', 'value']
+    mock_r = [
+        (1, 'D1', 1),
+        (2, 'D1', 2),
+        (3, 'D1', 2),
+        (4, 'D1', 3),
+        (5, 'D2', 1),
+        (6, 'D2', 1),
+        (7, 'D2', 2),
+        (8, 'D2', 3),
+    ]
+    df = pd.DataFrame().from_records(
+        mock_r, columns=mock_c).set_index('Entry Id')
+    return df
+
+
+def test_data_for_visualizer(monkeypatch):
+    exp = {'title': 'QC2',
+           'levels': ['1', '2', '3'],
+           'overall_f': [.375, .375, 0.25],
+           'dimensions': [
+               {'name': 'D1',
+                'freq': [0.25, 0.5, 0.25]},
+               {'name': 'D2',
+                'freq': [0.5, 0.25, 0.25]}
+           ]
+           }
+    monkeypatch.setattr(
+        Processor, 'dimension_value_frame', mock_dim_value_frame)
+
+    p = Processor()
+    res = p.data_for_visualizer('QC1', 'QC2')
+    assert res == exp
